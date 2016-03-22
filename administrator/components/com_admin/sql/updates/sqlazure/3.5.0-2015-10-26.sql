a1953348b09fa6a3d991e209b607e354edb46a22
Nicolas OGIER <nicolas.ogier@gmail.com>
2016-03-22 15:17:18 +0100

    Nettoyage pour branche 3.5 à partir de la version 3.5.0

--- administrator/components/com_admin/sql/updates/sqlazure/3.5.0-2015-10-26.sql
+++
@@ -1,3 +0,0 @@
-AlTER TABLE [#__contentitem_tag_map] DROP INDEX [idx_tag_name];
-AlTER TABLE [#__contentitem_tag_map] DROP INDEX [idx_tag];
-AlTER TABLE [#__contentitem_tag_map] DROP INDEX [idx_type];
