877ed500e669b1a9df22413ac2aefbb8ac02def8
Nicolas OGIER <nicolas.ogier@gmail.com>
2016-10-22 12:38:21 +0200

    Mise à jour de la version de développement 22/10/2016-12:38:07

--- layouts/joomla/tinymce/buttons/button.php
+++ layouts/joomla/tinymce/buttons/button.php
@@ -11,3 +11,5 @@ defined('JPATH_BASE') or die;
 
 JLog::add('The layout joomla.tinymce.buttons.button is deprecated, use joomla.editors.buttons.button instead.', JLog::WARNING, 'deprecated');
 echo JLayoutHelper::render('joomla.editors.buttons.button', $displayData);
+
+?>
