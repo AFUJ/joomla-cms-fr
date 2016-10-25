0e057a1ad687e980f31b3fee85fa1d8c0d33b296
Nicolas OGIER <nicolas.ogier@gmail.com>
2016-10-25 16:27:50 +0200

    mise à jour à partir des fichiers anglais

--- media/jui/js/cms-uncompressed.js
+++ media/jui/js/cms-uncompressed.js
@@ -8,9 +8,6 @@ if (typeof(Joomla) === 'undefined') {
 	var Joomla = {};
 }
 
-!(function (document, Joomla) {
-	"use strict";
-
 /**
  * Sets the HTML of the container-collapse element
  */
@@ -18,7 +15,7 @@ Joomla.setcollapse = function(url, name, height) {
     if (!document.getElementById('collapse-' + name)) {
         document.getElementById('container-collapse').innerHTML = '<div class="collapse fade" id="collapse-' + name + '"><iframe class="iframe" src="' + url + '" height="'+ height + '" width="100%"></iframe></div>';
     }
-};
+}
 
 /**
  * IE8 polyfill for indexOf()
@@ -47,45 +44,30 @@ if (!Array.prototype.indexOf)
 		return -1;
 	};
 }
-	/**
-	 * JField 'showon' feature.
-	 */
-	window.jQuery && (function ($) {
 
-		/**
-		 * Method to check condition and change the target visibility
-		 * @param {jQuery}  target
-		 * @param {Boolean} animate
-		 */
-		function linkedoptions (target, animate) {
-			var showfield = true,
-				jsondata  = target.data('showon') || [],
-				itemval, condition, fieldName, $fields;
+if (jQuery) {
+	jQuery(document).ready(function($) {
+		var linkedoptions = function(target) {
+			var showfield = true, itemval, jsondata = target.data('showon');
 
 			// Check if target conditions are satisfied
-			for (var j = 0, lj = jsondata.length; j < lj; j++) {
-				condition  = jsondata[j] || {};
-				fieldName  = condition.field;
-				$fields    = $('[name="' + fieldName + '"], [name="' + fieldName + '[]"]');
-
-				condition['valid'] = 0;
+			$.each(jsondata, function(j, item) {
+				$fields = $('[name="' + jsondata[j]['field'] + '"], [name="' + jsondata[j]['field'] + '[]"]');
+				jsondata[j]['valid'] = 0;
 
 				// Test in each of the elements in the field array if condition is valid
 				$fields.each(function() {
-					var $field = $(this);
-
 					// If checkbox or radio box the value is read from proprieties
-					if (['checkbox','radio'].indexOf($field.attr('type')) !== -1)
+					if (['checkbox','radio'].indexOf($(this).attr('type')) != -1)
 					{
-						itemval = $field.prop('checked') ? $field.val() : '';
+						itemval = $(this).prop('checked') ? $(this).val() : '';
 					}
 					else
 					{
-						itemval = $field.val();
+						itemval = $(this).val();
 					}
 
-					// Convert to array to allow multiple values in the field (e.g. type=list multiple)
-					// and normalize as string
+					// Convert to array to allow multiple values in the field (e.g. type=list multiple) and normalize as string
 					if (!(typeof itemval === 'object'))
 					{
 						itemval = JSON.parse('["' + itemval + '"]');
@@ -94,18 +76,18 @@ if (!Array.prototype.indexOf)
 					// Test if any of the values of the field exists in showon conditions
 					for (var i in itemval)
 					{
-						if (condition['values'].indexOf(itemval[i]) !== -1)
+						if (jsondata[j]['values'].indexOf(itemval[i]) != -1)
 						{
-							condition['valid'] = 1;
+							jsondata[j]['valid'] = 1;
 						}
 					}
 				});
 
 				// Verify conditions
 				// First condition (no operator): current condition must be valid
-				if (condition['op'] === '')
+				if (jsondata[j]['op'] == '')
 				{
-					if (condition['valid'] === 0)
+					if (jsondata[j]['valid'] == 0)
 					{
 						showfield = false;
 					}
@@ -114,88 +96,35 @@ if (!Array.prototype.indexOf)
 				else
 				{
 					// AND operator: both the previous and current conditions must be valid
-					if (condition['op'] === 'AND' && condition['valid'] + jsondata[j-1]['valid'] < 2)
+					if (jsondata[j]['op'] == 'AND' && jsondata[j]['valid'] + jsondata[j-1]['valid'] < 2)
 					{
 						showfield = false;
 					}
 					// OR operator: one of the previous and current conditions must be valid
-					if (condition['op'] === 'OR'  && condition['valid'] + jsondata[j-1]['valid'] > 0)
+					if (jsondata[j]['op'] == 'OR'  && jsondata[j]['valid'] + jsondata[j-1]['valid'] > 0)
 					{
 						showfield = true;
 					}
 				}
-			}
+			});
 
 			// If conditions are satisfied show the target field(s), else hide
-			if (animate) {
-				(showfield) ? target.slideDown() : target.slideUp();
-			} else {
-				target.toggle(showfield);
-			}
-		}
+			(showfield) ? target.slideDown() : target.slideUp();
+		};
 
-		/**
-		 * Method for setup the 'showon' feature, for the fields in given container
-		 * @param {HTMLElement} container
-		 */
-		function setUpShowon (container) {
-			container = container || document;
+		$('[data-showon]').each(function() {
+			var target = $(this), jsondata = $(this).data('showon');
 
-			var $showonFields = $(container).find('[data-showon]');
-
-			// Setup each 'showon' field
-			for (var is = 0, ls = $showonFields.length; is < ls; is++) {
-				// Use anonymous function to capture arguments
-				(function () {
-					var $target = $($showonFields[is]), jsondata = $target.data('showon') || [],
-						field, $fields = $();
-
-					// Collect an all referenced elements
-					for (var ij = 0, lj = jsondata.length; ij < lj; ij++) {
-						field   = jsondata[ij]['field'];
-						$fields = $fields.add($('[name="' + field + '"], [name="' + field + '[]"]'));
-					}
-
-					// Check current condition for element
-					linkedoptions($target);
-
-					// Attach events to referenced element, to check condition on change
-					$fields.on('change', function() {
-						linkedoptions($target, true);
-					});
-				})();
-			}
-		}
-
-		/**
-		 * Initialize 'showon' feature
-		 */
-		$(document).ready(function() {
-			setUpShowon();
-
-			// Setup showon feature in the subform field
-			$(document).on('subform-row-add', function(event, row){
-				var $row = $(row),
-					$elements = $row.find('[data-showon]'),
-					baseName  = $row.data('baseName'),
-					group     = $row.data('group'),
-					search    = new RegExp('\\[' + baseName + '\\]\\[' + baseName + 'X\\]', 'g'),
-					replace   = '[' + baseName + '][' + group + ']',
-					$elm, showon;
-
-				// Fix showon field names in a current group
-				for (var i = 0, l = $elements.length; i < l; i++) {
-					$elm   = $($elements[i]);
-					showon = $elm.attr('data-showon').replace(search, replace);
-
-					$elm.attr('data-showon', showon);
-				}
-
-				setUpShowon(row);
+			// Attach events to referenced element
+			$.each(jsondata, function(j, item) {
+				$fields = $('[name="' + jsondata[j]['field'] + '"], [name="' + jsondata[j]['field'] + '[]"]');
+				// Attach events to referenced element
+				$fields.each(function() {
+					linkedoptions(target);
+				}).bind('change', function() {
+					linkedoptions(target);
+				});
 			});
 		});
-
-	})(jQuery);
-
-
-})(document, Joomla);
+	});
+}
