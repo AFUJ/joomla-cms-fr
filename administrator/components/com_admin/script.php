94c2fc83f1bcd1073fecb57d54ca3a8deae6c941
Nicolas OGIER <nicolas.ogier@joomla.fr>
2016-11-04 17:55:06 +0100

    Ajout des fichiers de langue française site.

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -242,7 +242,6 @@ class JoomlaInstallerScript
 			array('component', 'com_contenthistory', '', 1),
 			array('component', 'com_postinstall', '', 1),
 			array('component', 'com_joomlaupdate', '', 1),
-			array('component', 'com_fields', '', 1),
 
 			// Libraries
 			array('library', 'phputf8', '', 0),
@@ -350,8 +349,6 @@ class JoomlaInstallerScript
 			array('plugin', 'phpversioncheck', 'quickicon', 0),
 			array('plugin', 'menu', 'editors-xtd', 0),
 			array('plugin', 'contact', 'editors-xtd', 0),
-			array('plugin', 'fields', 'system', 0),
-			array('plugin', 'gallery', 'fields', 0),
 
 			// Templates
 			array('template', 'beez3', '', 0),
@@ -1452,7 +1449,7 @@ class JoomlaInstallerScript
 			'/media/editors/tinymce/plugins/compat3x/editable_selects.js',
 			'/media/editors/tinymce/plugins/compat3x/form_utils.js',
 			'/media/editors/tinymce/plugins/compat3x/mctabs.js',
-			'/media/editors/tinymce/plugins/compat3x/tiny_mce_popup.js',
+			'/media/editors/tinymce/plugins/compat3x/tiny_mce_popup.js', 
 			'/media/editors/tinymce/plugins/compat3x/validate.js',
 			'/libraries/vendor/symfony/yaml/Symfony/Component/Yaml/Dumper.php',
 			'/libraries/vendor/symfony/yaml/Symfony/Component/Yaml/Escaper.php',
@@ -1698,8 +1695,7 @@ class JoomlaInstallerScript
 			'com_tags',
 			'com_contenthistory',
 			'com_ajax',
-			'com_postinstall',
-			'com_fields',
+			'com_postinstall'
 		);
 
 		foreach ($newComponents as $component)
