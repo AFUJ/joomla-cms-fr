ce5598a71904bec0b4744fb76ad45104d6848621
Nicolas OGIER <nicolas.ogier@gmail.com>
2017-04-11 00:08:41 +0200

    Modification du fichier joomla.sql, ajout des fichiers fr d'exemples, suppression des fichiers anglais.

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -1428,7 +1428,7 @@ CREATE TABLE IF NOT EXISTS `#__menu_types` (
 --
 
 INSERT IGNORE INTO `#__menu_types` (`id`, `asset_id`, `menutype`, `title`, `description`, `client_id`) VALUES
-(1, 0, 'mainmenu', 'Main Menu', 'The main menu for the site', 0);
+(1, 0, 'mainmenu', 'Menu principal', 'Le menu principal du site', 0);
 
 -- --------------------------------------------------------
 
