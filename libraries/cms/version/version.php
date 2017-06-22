6f09b226908cc14a9351970fcafc81e31143c331
Administrator <nicolas.ogier@gmail.com>
2017-06-22 20:09:52 +0200

    mise à jour à partir des fichiers anglais

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -38,7 +38,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_LEVEL = '3-dev';
+	const DEV_LEVEL = '3-rc1';
 
 	/**
 	 * Development status.
@@ -46,7 +46,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Development';
+	const DEV_STATUS = 'Release Candidate';
 
 	/**
 	 * Build number.
@@ -78,7 +78,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '20:18';
+	const RELTIME = '20:02';
 
 	/**
 	 * Release timezone.
