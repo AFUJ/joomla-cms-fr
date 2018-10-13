f1885817e24e96a6eccae9a1c097d21000297fff
Nicolas Ogier <nicolas.ogier@gmail.com>
2018-10-13 12:45:52 +0200

    mise à jour à partir des fichiers anglais

--- administrator/modules/mod_menu/menu.php
+++ administrator/modules/mod_menu/menu.php
@@ -323,10 +323,6 @@ class JAdminCssMenu
 			{
 				continue;
 			}
-			elseif ($item->element === 'com_joomlaupdate' && !$user->authorise('core.admin'))
-			{
-				continue;
-			}
 			elseif ($item->element === 'com_admin')
 			{
 				parse_str($item->link, $query);
