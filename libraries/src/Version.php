f8ddda63aab4c9335dfc231cfffa6ed1ad0e88c1
Opware2000 <opware2000@gmail.com>
2018-11-24 17:50:18 +0100

    Mise à jour de la version de développement 24/11/2018-17:50:01

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -111,7 +111,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '30-October-2018';
+	const RELDATE = '24-November-2018';
 
 	/**
 	 * Release time.
@@ -119,7 +119,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '14:00';
+	const RELTIME = '16:30';
 
 	/**
 	 * Release timezone.
