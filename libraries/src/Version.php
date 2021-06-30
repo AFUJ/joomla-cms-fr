8e5f0dc227fd1dae61d95fed93a2f3ed435fcf26
Opware2000 <opware2000@gmail.com>
2021-06-30 12:31:13 +0200

    Mise à jour de la version de développement 30/06/2021-12:30:01

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -111,7 +111,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '24-May-2021';
+	const RELDATE = '30-June-2021';
 
 	/**
 	 * Release time.
@@ -119,7 +119,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '11:29';
+	const RELTIME = '11:25';
 
 	/**
 	 * Release timezone.
